21188b843699347ffe9677e2ebbdc780c90d7479
mariampoke243-art <mariampoke243@gmail.com>
2026-09-13 16:06:55 +0100

    Update index.ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,13 +1,12 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+// Vous pouvez retirer ou garder LanguageDetector selon votre besoin, mais retirez 'lng: "en"'
 import messages from './local/index';
 
 i18n
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    lng: 'en',
+    // lng: 'en', // Supprimé pour éviter de verrouiller la langue
     fallbackLng: 'en',
     debug: false,
     resources: messages,
