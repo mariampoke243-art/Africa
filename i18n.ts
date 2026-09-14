a7dcaabca124a4d9b3c758176c8057ac8e7c7fbd
mariampoke243-art <mariampoke243@gmail.com>
2026-09-14 01:10:02 +0100

    Update i18n.ts

--- i18n.ts
+++ i18n.ts
@@ -1,22 +1,19 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
+import resources from './i18n/local/index'; // Adaptez le chemin selon l'emplacement exact de i18n.ts par rapport à src/i18n/local
 
 i18n
-  .use(HttpApi)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    supportedLngs: ['en', 'fr'],
+    resources,
+    supportedLngs: ['en', 'fr', 'es', 'pt', 'zh'],
     fallbackLng: 'en',
     debug: process.env.NODE_ENV === 'development',
     detection: {
       order: ['path', 'cookie', 'htmlTag', 'localStorage', 'subdomain'],
       caches: ['cookie'],
-    },
-    backend: {
-      loadPath: '/locales/{{lng}}/translation.json',
     },
     react: {
       useSuspense: true,
