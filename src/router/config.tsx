c89d7534e8ac83d1fa27aa6352861e90aeceb01e
mariampoke243-art <mariampoke243@gmail.com>
2026-09-14 16:17:03 +0100

    Update config.tsx

--- src/router/config.tsx
+++ src/router/config.tsx
@@ -1,7 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import AgendaPage from '../pages/AgendaPage';
+// Importez votre page d'accueil ou composant principal si nécessaire
+// import HomePage from '../pages/HomePage';
 
 export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AgendaPage />, // ou un Layout / HomePage
+  },
   {
     path: '/agenda',
     element: <AgendaPage />,
