62c62e94cfe2c7c637895fdac6b606da528c54fd
mariampoke243-art <mariampoke243@gmail.com>
2026-09-19 08:04:49 +0100

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { LanguageSelector } from './LanguageSelector';
 
 export const Layout: React.FC = () => {
+  const location = useLocation();
+
+  const isHomePage = location.pathname === '/';
+
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="flex justify-between items-center px-6 py-4 border-b">
-        <div className="font-bold text-lg">Africa</div>
-        <LanguageSelector />
-      </header>
+      {!isHomePage && (
+        <header className="flex justify-between items-center px-6 py-4 border-b bg-white">
+          <div className="font-bold text-lg">Africa</div>
+
+          <LanguageSelector />
+        </header>
+      )}
+
       <main className="flex-1">
         <Outlet />
       </main>
